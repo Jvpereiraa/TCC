d3d56119903293b9a374f50d258dc1b6f6cf179f
Jvpereiraa <jvpereiraa97@gmail.com>
2019-03-26 18:03:45 -0300

    Teste Heroku

--- src/main/java/br/com/kodeweave/conf/JPAProductionConfiguration.java
+++ src/main/java/br/com/kodeweave/conf/JPAProductionConfiguration.java
@@ -14,6 +14,7 @@ import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Profile("prod")
+@EnableTransactionManagement
 public class JPAProductionConfiguration {
 
     @Autowired
