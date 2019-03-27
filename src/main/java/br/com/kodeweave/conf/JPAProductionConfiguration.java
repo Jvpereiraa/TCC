40570bc783d612dae3cc48872027f103d6ed4c22
Jvpereiraa <jvpereiraa97@gmail.com>
2019-03-27 17:43:45 -0300

    Teste Heroku

--- src/main/java/br/com/kodeweave/conf/JPAProductionConfiguration.java
+++ src/main/java/br/com/kodeweave/conf/JPAProductionConfiguration.java
@@ -4,8 +4,6 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.Properties;
 
-import javax.sql.DataSource;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Profile;
@@ -13,39 +11,33 @@ import org.springframework.core.env.Environment;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-@Profile("prod")
 @EnableTransactionManagement
+@Profile("prod")
 public class JPAProductionConfiguration {
+	
+	@Autowired
+	private Environment environment;
 
-    @Autowired
-    private Environment environment;
+	@Bean
+	public Properties additionalProperties() {
+		Properties properties = new Properties();
+		properties.setProperty("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
+		properties.setProperty("hibernate.show_sql", "true");
+		properties.setProperty("hibernate.hbm2ddl.auto", "update");
+		return properties;
+	}
 
-    @Bean
-    public Properties additionalProperties() {
-        Properties props = new Properties();
-        props.setProperty("hibernate.dialect", 
-            "org.hibernate.dialect.PostgreSQLDialect");
-        props.setProperty("hibernate.show_sql", "true");
-        props.setProperty("hibernate.hbm2ddl.auto", "update");
-        return props;
-    }
-
-    @Bean       
-    public DataSource dataSource() throws URISyntaxException {
-        DriverManagerDataSource dataSource = 
-            new DriverManagerDataSource();
-        dataSource.setDriverClassName("org.postgresql.Driver");
-
-        URI dbUrl = new URI(environment.getProperty("DATABASE_URL"));
-        dataSource.setUrl("jdbc:postgresql://" + dbUrl.getHost() + 
-            ":" + dbUrl.getPort() + dbUrl.getPath());
-        dataSource.setUsername(dbUrl.getUserInfo().split(":")[0]);
-        dataSource.setPassword(dbUrl.getUserInfo().split(":")[1]);
-
-        return dataSource;
-    }  
-    
-    
-    
-    
+	@Bean
+	private DriverManagerDataSource dataSource() throws URISyntaxException {
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName("org.postgresql.Driver");
+	    
+	    URI dbUrl = new URI(environment.getProperty("DATABASE_URL"));
+	    
+	    dataSource.setUrl("jdbc:postgresql://"+dbUrl.getHost()+":"+dbUrl.getPort()+dbUrl.getPath());
+	    dataSource.setUsername(dbUrl.getUserInfo().split(":")[0]);
+	    dataSource.setPassword(dbUrl.getUserInfo().split(":")[1]);
+	    
+		return dataSource;
+	}
 }
